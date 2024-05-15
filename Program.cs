29e1e8236af010f44213b0f8e4743462b53ac1e5
Nakhid Usta <nakhidusta@gmail.com>
2024-05-15 09:41:47 +0400

    Admin-Panel FronttoBack integrated

--- Program.cs
+++ Program.cs
@@ -7,5 +7,5 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Conf
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
-
+app.MapControllerRoute("admin", "{area:exists}/{controller=dashboard}/{action=index}/{id?}");
 app.Run();
