37195dbd9d4d90766c735f49efa169d1b5a46f30
Nakhid Usta <nakhidusta@gmail.com>
2024-05-17 13:54:10 +0400

    Created Update,delete category

--- Areas/Manage/Controllers/CategoryController.cs
+++ Areas/Manage/Controllers/CategoryController.cs
@@ -27,8 +27,54 @@ namespace Pronia.Areas.Manage.Controllers
         public async Task<IActionResult> Create(Category category)
         {
             if(!ModelState.IsValid) { return View(category); }
+            if((await context.Categories.AnyAsync(c => c.Name == category.Name)))
+            {
+                ModelState.AddModelError("Name", "Category with this name already exists!");
+                return View();
+            }
             await context.Categories.AddAsync(category);
             await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+             Category? category= await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id,Category category)
+        {
+            if (id <= 0) return BadRequest();
+          Category? categoryFromDb= await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if(categoryFromDb is null) return NotFound();
+            if ((await context.Categories.AnyAsync(c => c.Name == category.Name)))
+            {
+                ModelState.AddModelError("Name", "Category with this name already exists!");
+                return View();
+            }
+           categoryFromDb.Name = category.Name ;
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+          
+           
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Category? categoryFromDb = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            context.Categories.Remove(categoryFromDb);
+             await   context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
